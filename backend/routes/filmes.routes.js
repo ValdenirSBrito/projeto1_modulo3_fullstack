5616e5c4c8163761088333575443493c9092c53d
Valdenir Silva Brito <59507605+ValdenirSBrito@users.noreply.github.com>
2021-11-02 22:13:53 -0400

    new

--- backend/routes/filmes.routes.js
+++ backend/routes/filmes.routes.js
@@ -12,6 +12,16 @@ const filmes = [
         descricao: 'Em Velozes & Furiosos 9, Dominic Toretto (Vin Diesel) e Letty (Michelle Rodriguez) vivem uma vida pacata ao lado de seu filho Brian. Mas eles logo são ameaçados quando o irmão desaparecido de Dom retorna. Jakob (John Cena), um assassino habilidoso e excelente motorista, está trabalhando ao lado de Cipher (Charlize Theron), vilã de Velozes & Furiosos 8. Para enfrentá-los, Toretto vai precisar reunir sua equipe novamente, inclusive Han (Sung Kang), que todos acreditavam estar morto.',
         assistido: false
     },
+
+    {
+        id: 2,
+        nome: 'Velozes e Furiosos 7',
+        imagem: 'https://br.web.img3.acsta.net/c_310_420/pictures/15/03/30/21/19/054397.jpg',
+        genero: 'Ação',
+        nota: 8,
+        descricao: 'Velozes e Furiosos 7 acompanha Dom (Vin Diesel), Brian (Paul Walker), Letty (Michelle Rodriguez) e o resto da equipe após os acontecimentos em Londres. Apesar de terem suas chances de voltar para os Estados Unidos e recomeçarem suas vidas, a tranquilidade do grupo é destruída quando Deckard Shaw (Jason Statham), um assassino profissional, quer vingança pela morte de seu irmão. Agora, a equipe tem que se reunir para impedir este novo vilão. Mas dessa vez, não é só sobre ser veloz. A luta é pela sobrevivência.',
+        assistido: false
+    },
 ]
 
 // [GET] /vagas - Retornar uma lista de vagas
